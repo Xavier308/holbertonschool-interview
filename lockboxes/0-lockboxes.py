f62a1613094438c0bb1fb23c063d3243b3a9773c
xavierdev <xavvis308@gmail.com>
2025-01-03 10:54:13 -0400

    module documented

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python3
+"""Module for determining if all boxes can be unlocked.
+
+This module provides a function `canUnlockAll` that checks if all
+nested boxes can be unlocked using the keys contained within them.
+"""
 
 def canUnlockAll(boxes):
+    """Determine if all boxes can be unlocked.
+
+    Starting from the first box, which is initially unlocked,
+    this function uses a breadth-first search (BFS) approach
+    to try to unlock all subsequent boxes using the keys found
+    in each box.
+
+    Args:
+        boxes (list of lists of int): The list of boxes with each box containing a list of key indices.
+
+    Returns:
+        bool: True if all boxes can be unlocked, False otherwise.
+    """
     if not boxes:
         return False
     
