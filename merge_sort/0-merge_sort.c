365a07341b7e85565466996ce056edb313723ea3
xavierdev <xavvis308@gmail.com>
2025-06-09 15:57:47 -0400

    module documentation fix

--- merge_sort/0-merge_sort.c
+++ merge_sort/0-merge_sort.c
@@ -3,6 +3,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/**
+ * merge_subarray - merges two sorted halves of an array
+ * @array: pointer to the array containing subarrays
+ * @start: starting index of the first subarray
+ * @mid: starting index of the second subarray
+ * @end: index one past the end of the second subarray
+ * @buffer: auxiliary buffer for merged output
+ */
 static void merge_subarray(int *array, size_t start,
 	size_t mid, size_t end, int *buffer)
 {
@@ -40,6 +49,13 @@ static void merge_subarray(int *array, size_t start,
 	print_array(array + start, left_size + right_size);
 }
 
+/**
+ * merge_sort_recursive - recursively splits and sorts array segments
+ * @array: pointer to the array to sort
+ * @start: starting index of the segment
+ * @end: index one past the end of the segment
+ * @buffer: auxiliary buffer for merging
+ */
 static void merge_sort_recursive(int *array, size_t start,
 	size_t end, int *buffer)
 {
@@ -54,6 +70,13 @@ static void merge_sort_recursive(int *array, size_t start,
 		start + (end - start) / 2, end, buffer);
 }
 
+
+/**
+ * merge_sort - sorts an array of integers in ascending order
+ *               using the Merge Sort algorithm
+ * @array: pointer to the first element of the array to sort
+ * @size: number of elements in the array
+ */
 void merge_sort(int *array, size_t size)
 {
 	int *buffer;
